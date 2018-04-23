601a0b4ca78495367b3d86ab6ce75d26f2fa02e7
Pankaj <pankaj.yogi@gmail.com>
2018-04-23 15:49:43 +0530

    ewf

---
+++ src/com/maventest/mavens/AssertVsVerify1.java
@@ -0,0 +1,140 @@
+package com.maventest.mavens;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.Reporter;
+import org.testng.TestRunner;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.beust.testng.TestNG;
+
+public class AssertVsVerify1 {
+	static WebDriver driver ;
+	@Test(groups={"sampleTest01"})
+	public void testNG() throws MalformedURLException {
+		//System.setProperty("webdriver.chrome.driver","\\Grid\\chromedriver.exe");
+		//driver = new ChromeDriver();
+		String GridURL="http://localhost:4444/wd/hub";
+		DesiredCapabilities capability = new DesiredCapabilities();
+		capability = DesiredCapabilities.chrome();
+		capability.setCapability("chrome.switches", Arrays.asList("--disable-extensions"));
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-extensions");
+		
+		ArrayList<String> excludeSwitches = new ArrayList<String>();
+		excludeSwitches.add("ignore-certificate-errors"); 
+		options.setExperimentalOption("excludeSwitches", excludeSwitches);		
+		capability.setCapability(ChromeOptions.CAPABILITY, options);
+		driver = new RemoteWebDriver(new URL(GridURL), capability);
+		
+		Report("Driver Launched");
+		String baseUrl = "http://opensource.demo.orangehrmlive.com/";
+		String expectedTitle = "OrangeHRM1";
+		String actualTitle = "";
+		String ExpectedWelcomeUser="Welcome Admin1";
+		driver.get(baseUrl);
+		Report("Navigated to"+baseUrl);
+		actualTitle = driver.getTitle();
+		login();
+		WebElement welcome= driver.findElement(By.linkText("Welcome Admin"));
+		String ActualWelcomeUser= welcome.getText();
+		try{
+			//verify(actualTitle, expectedTitle);
+
+			//verify(ActualWelcomeUser, ExpectedWelcomeUser);
+			 /*....................................Verify Vs Assert.........................................................	
+*/
+			hardAssert(ActualWelcomeUser, ExpectedWelcomeUser);
+
+			//hardAssert(actualTitle, expectedTitle);
+
+			softAssert(ActualWelcomeUser, ExpectedWelcomeUser);
+
+			softAssert(actualTitle, expectedTitle);
+
+		}catch(Exception e){
+
+			Report("Test case execution stops as checkpoint failed- HardAssert used");
+
+		}
+		exitApplication();
+	}
+
+	public void verify(String actualTitle, String expectedTitle){
+
+		if (actualTitle.contentEquals(expectedTitle)){
+			Report("Test Passed!");
+		} else {
+			Report("Test Failed");
+		}
+		Report("Verify continues the execution even if checkpoint get failed");
+
+	}
+
+	public void hardAssert(String actualTitle, String expectedTitle){
+
+		Assert.assertEquals(actualTitle, expectedTitle);
+
+	}
+	public void softAssert(String actualTitle, String expectedTitle){
+		try{
+
+			Assert.assertEquals(actualTitle, expectedTitle);
+
+		}catch(AssertionError e){
+
+			e.printStackTrace();
+
+		}
+	}
+
+	public void login(){
+		WebElement userName= driver.findElement(By.id("txtUsername"));//By id 	
+		WebElement password= driver.findElement(By.name("txtPassword"));//By name 
+		WebElement submit= driver.findElement(By.className("button"));//By name 	
+		userName.sendKeys("Admin");
+		Report("UserName entered "+ "Admin");
+		password.sendKeys("admin");
+		Report("Password entered "+ "admin");
+		submit.click();
+		Report("submit button clicked");
+	}
+
+	public void exitApplication(){
+		driver.close();
+
+		Report("Test case executed successfully");
+
+	}
+
+	public void Report(String s){
+
+		Reporter.log(s);
+
+	}
+	@BeforeSuite
+	public void GenReport(ITestContext ctx){
+
+		System.out.println("In after suite");
+		TestRunner runner = (TestRunner) ctx;
+		runner.setOutputDirectory("C:\\Tosca");
+
+
+	}
+
+}
